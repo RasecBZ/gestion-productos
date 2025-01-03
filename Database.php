f840c5147a80728830007f136c4c7573de078ca8
RasecBZ <raseccbz@gmail.com>
2025-01-02 19:39:09 -0700

    Database.php

--- Database.php
+++ Database.php
@@ -1,82 +1,57 @@
 <?php
 
 class Database
-{        
+{
     private $_connection;
-    private static $_instance; // La instancia única
-    private $_host = "localhost";
-    private $_username = "root";
-    private $_password = "";
-    private $_database = "tarea1";  // Nombre de tu base de datos
+    private static $_instance;
+    private $_host = "sql309.infinityfree.com"; // Host de InfinityFree
+    private $_username = "if0_38007614";       // Usuario de InfinityFree
+    private $_password = "Amaya1603";           // Contraseña de InfinityFree
+    private $_database = "if0_38007614_proyecto_final"; // Nombre de la base de datos
 
-    /*
-    Obtener una instancia de la base de datos
-    @return Instance
-    */
     public static function getInstance()
     {
-        if(!self::$_instance) // Si no existe una instancia, crear una
-        { 
+        if (!self::$_instance) { 
             self::$_instance = new self();
         }
         return self::$_instance;
     }
 
-    // Constructor
     public function __construct()
     {
+        if ($_SERVER['SERVER_NAME'] === 'localhost') {
+            $this->_host = "localhost";
+            $this->_username = "root";
+            $this->_password = "";
+            $this->_database = "tarea1";
+        } else {
+            $this->_host = "sql309.infinityfree.com"; // Host de InfinityFree
+            $this->_username = "if0_38007614";       // Usuario de InfinityFree
+            $this->_password = "Amaya1603";           // Contraseña de InfinityFree
+            $this->_database = "if0_38007614_proyecto_final"; // Base de datos
+        }
+
         $this->_connection = new mysqli($this->_host, $this->_username, $this->_password, $this->_database);
-        // Manejo de errores
-        if(mysqli_connect_error())
-        {
-            trigger_error("Error al conectar con MySQL: " . mysqli_connect_error(), E_USER_ERROR);
+
+        if ($this->_connection->connect_error) {
+            die("Conexión fallida: " . $this->_connection->connect_error);
         }
     }
 
-    // Método para prevenir la clonación de la conexión
-    public function __clone() { }
-
-    // Obtener la conexión mysqli
     public function getConnection()
     {
         return $this->_connection;
     }
 
-    // Ejecutar una consulta SELECT y obtener los datos
-    public function get_data($sql)
-    {
-        $ret = array('STATUS'=>'ERROR', 'ERROR'=>'', 'DATA'=>array());
-
-        $mysqli = $this->getConnection();
-        $res = $mysqli->query($sql);
-
-        if($res)
-            $ret['STATUS'] = "OK";
-        else
-            $ret['ERROR'] = mysqli_error();
-
-        while($row = $res->fetch_array(MYSQLI_ASSOC))
-        {
-            $ret['DATA'][] = $row;
-        }
-        return $ret;
-    }
-
-    // Ejecutar cualquier consulta (INSERT, UPDATE, DELETE)
     public function exec($sql)
     {
-        $ret = array('STATUS'=>'ERROR', 'ERROR'=>'');
-
         $mysqli = $this->getConnection();
         $res = $mysqli->query($sql);
-
-        if($res)
-            $ret['STATUS'] = "OK";
-        else
-            $ret['ERROR'] = mysqli_error();
-
-        return $ret;
+        if ($res) {
+            return true;
+        } else {
+            return false;
+        }
     }
-} // Esta es la llave de cierre que estaba faltando
-
+}
 ?>
